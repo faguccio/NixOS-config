ea4073e1c0497ec010fd2674932da7b093669edd
Fabio Gaiba <faga2703@gmail.com>
2024-03-23 16:02:37 +0100

    First config

---
+++ systempackages.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{ 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    firefox
+    vscodium
+    git
+    alejandra
+    neofetch
+    htop
+  ];
+
+}
