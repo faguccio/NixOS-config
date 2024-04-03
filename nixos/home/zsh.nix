22c41194cccbeed956308a4877d8d1d77b41cf29
Fabio Gaiba <faga2703@gmail.com>
2024-04-03 17:24:03 +0200

    92 current  2024-04-03 17:23:58  23.11.5541.56528ee42526  6.1.82                          *

--- nixos/home/zsh.nix
+++ nixos/home/zsh.nix
@@ -1,54 +1,59 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  #environment.pathsToLink = ["/share/zsh"];
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  #environment.shells = with pkgs; [zsh];
-
   home-manager.users.f4g4 = {
-    # home.packages = with pkgs; [
-    # ];
-
     programs.zsh = {
-      enableCompletion = true;
-      #enableAutosuggestions = true;
-      syntaxHighlighting.enable = true;
-
-      shellAliases = {
-        ll = "ls -l";
-      };
-      history.size = 10000;
-      historySubstringSearch.enable = true;
-      #history.path = "${config.xdg.dataHome}/zsh/history";
-      initExtra = ''
-        if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-          source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-        fi
-
-      '';
-      dotDir = ".config/zsh";
-
-      prezto = {
-        enable = true;
-        pmodules = [
-          "environment"
-          "terminal"
-          "editor"
-          "history"
-          "directory"
-          "spectrum"
-          "utility"
-          "completion"
-          "prompt"
-          "history-substring-search"
-          "git"
-          "syntax-highlighting"
-          "autosuggesitons"
-        ];
-      };
+      enable = true;
     };
   };
 }
+# {
+#   #environment.pathsToLink = ["/share/zsh"];
+#   #programs.zsh.enable = true;
+#   users.defaultUserShell = pkgs.zsh;
+#   #environment.shells = with pkgs; [zsh];
+#   home-manager.users.f4g4 = {
+#     # home.packages = with pkgs; [
+#     # ];
+#     programs.zsh = {
+#       enabletrue;
+#       enableCompletion = true;
+#       #enableAutosuggestions = true;
+#       syntaxHighlighting.enable = true;
+#       shellAliases = {
+#         ll = "ls -l";
+#       };
+#       history.size = 10000;
+#       historySubstringSearch.enable = true;
+#       #history.path = "${config.xdg.dataHome}/zsh/history";
+#       initExtra = ''
+#         if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+#           source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+#         fi
+#       '';
+#       dotDir = ".config/zsh";
+#       prezto = {
+#         enable = true;
+#         pmodules = [
+#           "environment"
+#           "terminal"
+#           "editor"
+#           "history"
+#           "directory"
+#           "spectrum"
+#           "utility"
+#           "completion"
+#           "prompt"
+#           "history-substring-search"
+#           "git"
+#           "syntax-highlighting"
+#           "autosuggesitons"
+#         ];
+#       };
+#     };
+#   };
+# }
+
