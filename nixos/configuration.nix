1075d8a4228cff8810993a48fbda686fa3bdcbe6
Fabio Gaiba <faga2703@gmail.com>
2024-04-20 18:09:03 +0200

    124 current  2024-04-20 18:08:59  23.11.5541.56528ee42526  6.1.82                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,6 +91,9 @@
   '';
 
   programs.light.enable = true;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; #
   services.blueman.enable = true;
 
   # For garbage colection
