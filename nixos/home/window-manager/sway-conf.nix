24714e5ffa14a841e2ce1f6a4a83fc9a16c9cb7e
Fabio Gaiba <faga2703@gmail.com>
2024-05-28 16:59:41 +0200

    174 current  2024-05-28 16:59:36  23.11.7093.44072e24566c  6.1.90                          *

--- nixos/home/window-manager/sway-conf.nix
+++ nixos/home/window-manager/sway-conf.nix
@@ -41,7 +41,7 @@ in {
 
           set $gnome-schema org.gnome.desktop.interface
 
-          exec swaymsg output eDP-1 scale 1.5
+          exec swaymsg output eDP-1 scale 2
 
           exec_always {
             gsettings set org.gnome.desktop.interface cursor-theme oreo_white_cursors
